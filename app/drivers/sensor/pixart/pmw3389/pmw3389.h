82eb09060a82e8a1a8d8285de076dcf92101ad9a
Gabraham <gwthmpsn@gmail.com>
2023-05-29 15:34:13 -0700

    Consolidating

---
+++ app/drivers/sensor/pixart/pmw3389/pmw3389.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef ZEPHYR_INCLUDE_PMW3389_H_
+#define ZEPHYR_INCLUDE_PMW3389_H_
+
+/**
+ * @file pmw3389.h
+ *
+ * @brief Header file for the pmw3389 driver.
+ */
+
+#include "../pixart.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* ZEPHYR_INCLUDE_PMW3389_H_ */
