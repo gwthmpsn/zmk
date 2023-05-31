15ca5036e683d83783270c2fe3b4406015283f0f
Gabraham <gwthmpsn@gmail.com>
2023-05-31 06:35:29 -0700

    Tried a different value for firmware ID

--- app/drivers/sensor/pixart/pmw3360/pmw3360.c
+++ app/drivers/sensor/pixart/pmw3360/pmw3360.c
@@ -79,7 +79,7 @@ LOG_MODULE_REGISTER(pmw3360, CONFIG_PMW3360_LOG_LEVEL);
 
 /* Sensor identification values */
 #define PMW3360_PRODUCT_ID			0x42
-#define PMW3360_FIRMWARE_ID			0x04
+#define PMW3360_FIRMWARE_ID			0xBD
 
 /* Max register count readable in a single motion burst */
 #define PMW3360_MAX_BURST_SIZE			12
